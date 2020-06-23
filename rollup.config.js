ebb7303eb76c1af466b9cd22ba74b04e65494126
Manuel Juan Fosela Águila <manufosela@gmail.com>
2020-06-23 20:23:26 +0200

    improvments

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,25 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
+import {
+  terser
+} from 'rollup-plugin-terser';
 
 export default {
   preserveSymlinks: true,
-	input: ['historical-line.js'],
-	output: {
-		file: 'build/historical-line.js',
+  input: [ 'historical-line.js' ],
+  output: {
+    file: 'build/historical-line.min.js',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: true
+  },
+  plugins: [
     resolve(),
-    babel()
+    babel(),
+    terser({
+      output: {
+        comments: false
+      },
+    })
   ]
 };
